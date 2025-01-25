cb733cc37c67ef0608f850413c81e0773b9fe154
lislis <mail@lislis.de>
2025-01-25 22:16:50 +0100

    update controller

--- frog_controller_keyboard/frog_controller_keyboard.ino
+++ frog_controller_keyboard/frog_controller_keyboard.ino
@@ -1,10 +1,7 @@
 #include <Keyboard.h>
 
 const int inPin = A1;
-int initThres;
 bool keyState = false;
-
-int threshold = 50;
 
 void setup() {
   Serial.begin(9600);
@@ -12,25 +9,19 @@ void setup() {
 }
 
 void loop() {
-	unsigned long t = millis();  // Get timestamp for comparison
 
   int reading = collectSample();
-  int remap = map(reading, initThres, 0, 0, threshold);
-
   //Serial.println(remap);
 
-  if (remap > (float)threshold * 0.8 && !keyState) {
+  if (reading > 0) {
     Keyboard.press('w');
     keyState = true;
-    delay(100);
+    delay(200);
   } else if (keyState) {
     Keyboard.release('w');
     keyState = false;
   }
 
-  if (reading > initThres) {
-    initThres = reading;
-  }       
   delay(100);                      
 }
   
