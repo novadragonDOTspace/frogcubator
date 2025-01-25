403d515dcf367e3f4f326d8d2031ed97f6e8724c
lislis <mail@lislis.de>
2025-01-25 23:38:50 +0100

    add rpm controller

---
+++ frog_controller_keyboard/frog_controller_rpm/frog_controller_rpm.ino
@@ -0,0 +1,35 @@
+//More information at: https://www.aeq-web.com/
+
+const int SensorPin = 2;  //Define Interrupt Pin (2 or 3 @ Arduino Uno)
+
+int InterruptCounter, rpm;
+
+void setup(){
+  delay(1000);
+  Serial.begin(9600);
+  Serial.print("Counting");
+}
+
+void loop() {
+  meassure();
+}
+
+void meassure() {
+  InterruptCounter = 0;
+  attachInterrupt(digitalPinToInterrupt(SensorPin), countup, RISING);
+  delay(1000);
+  detachInterrupt(digitalPinToInterrupt(SensorPin));
+  rpm = (InterruptCounter / 2) * 60;
+  display_rpm();
+}
+
+void countup() {
+  InterruptCounter++;
+}
+
+void display_rpm() {
+  Serial.print("Counts: ");
+  Serial.print(InterruptCounter, 1);
+  Serial.print(" RPM: ");
+  Serial.println(rpm);
+}
